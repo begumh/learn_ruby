6d9d5beacd8875217c163f7a401a325538687c98
Haleema Begum <haleema_ali13@yahoo.com>
2018-10-04 19:10:08 +0000

    Temperature

---
+++ 01_temperature/test.rb
@@ -0,0 +1,23 @@
+def ftoc(feh)
+    cel = 5*(feh-32)/9
+    puts cel 
+end
+def main 
+    puts "Enter temperature in Fehrenheit: "
+    feh = gets.chomp.to_f
+    
+    ftoc(feh)
+end
+main if __FILE__ ==$PROGRAM_NAME
+
+print "Enter a value of fehrenheit: "
+puts feh = gets.chomp.to_f
+if feh == {}
+    print "ENd"
+else 
+    cel = 5*(feh-32)/9
+    puts cel
+end 
+
+
+    
